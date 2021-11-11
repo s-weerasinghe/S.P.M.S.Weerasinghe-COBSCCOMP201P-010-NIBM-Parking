bd1ff45b01e63926f9bab42f284611d6e8e62f7a
s-weerasinghe <weerasinghesandaruwani@gmail.com>
2021-11-11 12:10:04 +0530

    Initial Commit

---
+++ NIBM Parking/NIBM_ParkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NIBM_ParkingApp.swift
+//  NIBM Parking
+//
+//  Created by sandaruwani weerasinghe on 2021-11-11.
+//
+
+import SwiftUI
+
+@main
+struct NIBM_ParkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
