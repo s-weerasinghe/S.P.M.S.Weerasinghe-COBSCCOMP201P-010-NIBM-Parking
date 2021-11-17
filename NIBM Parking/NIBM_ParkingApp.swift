c37856e77d4574f2cbd68f982881ce830e10321d
s-weerasinghe <weerasinghesandaruwani@gmail.com>
2021-11-17 08:03:28 +0530

    install firebase

--- NIBM Parking/NIBM_ParkingApp.swift
+++ NIBM Parking/NIBM_ParkingApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct NIBM_ParkingApp: App {
+    
+    init() {
+         FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
