d6b8232422269835a02fecfbe592648e184be3aa
s-weerasinghe <weerasinghesandaruwani@gmail.com>
2021-12-08 11:40:51 +0530

    reset password

---
+++ NIBM Parking/password reset.swift
@@ -0,0 +1,20 @@
+//
+//  password reset.swift
+//  NIBM Parking
+//
+//  Created by Sandaruwani Weerasinghe on 2021-12-08.
+//
+
+import SwiftUI
+
+struct password_reset: View {
+    var body: some View {
+        Text("forget password")
+    }
+}
+
+struct password_reset_Previews: PreviewProvider {
+    static var previews: some View {
+        password_reset()
+    }
+}
